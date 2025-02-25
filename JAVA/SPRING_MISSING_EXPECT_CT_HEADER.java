5bcea6e0e4bce92494e6ab9add8869d44991fb10
DGmikedev <dgmike.dev@gmail.com>
2025-02-25 14:03:49 -0600

    update javadocs

--- JAVA/SPRING_MISSING_EXPECT_CT_HEADER.java
+++ JAVA/SPRING_MISSING_EXPECT_CT_HEADER.java
@@ -6,26 +6,28 @@ protected void configure(HttpSecurity http) throws Exception {
     http.headers().addHeaderWriter(new StaticHeadersWriter("Expect-CT", "max-age=3600, enforce"));                      
 }
 
+// Agregar al POM: 
+
+ <dependency>
+     <groupId> 
+          org.springframework.boot 
+     </groupId>
+     <artifactId> spring-boot-starter-security </artifactId>
+ </dependency>
+ <dependency>
+     <groupId>
+         org.springframework.security
+     </groupId>
+     <artifactId>
+         spring-security-test
+     </artifactId>
+      <scope>
+         test
+     </scope>
+ </dependency>
+
 
 // descripción solución:
 //      Al no tener spring security en la api  
 //      Agregar las dependencias necesarias al POM y crear una clase que extienda de WebSecurityConfigurerAdapter	
  
-// Agregar al POM: 
-
-//     <dependency>
-//         <groupId> org.springframework.boot </groupId>
-//         <artifactId> spring-boot-starter-security </artifactId>
-//     </dependency>
-//     <dependency>
-//         <groupId>
-//             org.springframework.security
-//         </groupId>
-//         <artifactId>
-//             spring-security-test
-//         </artifactId>
-//          <scope>
-//             test
-//         </scope>
-//     </dependency>
-    
--- JAVA/USE_OF_HARDCODED_PASSWORD_IN_CONFIG_SURGE.java
+++ JAVA/USE_OF_HARDCODED_PASSWORD_IN_CONFIG_SURGE.java
@@ -7,52 +7,52 @@
 // al poner una constraseña no encriptada en el aplication.properties Utilizar 
 // algun cifrado valido para las contraseñas.	
 // 
-// Descargar jasypt en  http://www.jasypt.org/download.html 
+   Descargar jasypt en  http://www.jasypt.org/download.html 
 // 
 // Extraer la carpeta y colocarse en la carpeta bin ejecutar 
-//     encrypt.bat input="testers" 
-//     password="palabraclave"      // password es la palabra clave a utilizar para el cifrado.
-//     algorithm="PBEWITHHMACSHA384ANDAES_256" 
-//     ivGeneratorClassName="org.jasypt.iv.RandomIvGenerator"
-//     Donde: input es el password de la aplicación. 
+    encrypt.bat input="testers" 
+    password="palabraclave"      // password es la palabra clave a utilizar para el cifrado.
+    algorithm="PBEWITHHMACSHA384ANDAES_256" 
+    ivGeneratorClassName="org.jasypt.iv.RandomIvGenerator"
+    Donde: input es el password de la aplicación. 
 //     
 //     Esto nos dará la contraseña cifrada.  
 // 
 // 
-//     Dependencias y plugins en el POM:
+     Dependencias y plugins en el POM:
 
-//     <dependency>
-//         <groupId>
-//             com.github.USER
-//         </groupId>
-//         <artifactId>
-//             jasypt-spring-boot-starter
-//         </artifactId>
-//             <version>
-//                 3.0.3
-//             </version>
-//     </dependency>
-//         <plugin>
-//             <groupId>
-//                 com.github.USER
-//             </groupId>
-//             <artifactId>
-//                 jasypt-maven-plugin
-//             </artifactId>
-//             <version>
-//                 3.0.3
-//             </version>
-//         </plugin>
+     <dependency>
+         <groupId>
+             com.github.USER
+         </groupId>
+         <artifactId>
+             jasypt-spring-boot-starter
+         </artifactId>
+             <version>
+                 3.0.3
+             </version>
+     </dependency>
+         <plugin>
+             <groupId>
+                 com.github.USER
+             </groupId>
+             <artifactId>
+                 jasypt-maven-plugin
+             </artifactId>
+             <version>
+                 3.0.3
+             </version>
+         </plugin>
 //         
 //         En el aplication.properties colocar lo siguiente: 
-//             jasypt.encryptor.iv-generator-classname= org.jasypt.iv.RandomIvGenerator 
-//             jasypt.encryptor.password=palabraclave (palabra clave utilizada anteriormente) 
+             jasypt.encryptor.iv-generator-classname= org.jasypt.iv.RandomIvGenerator 
+             jasypt.encryptor.password=palabraclave (palabra clave utilizada anteriormente) 
 //             
-//             jasypt.encryptor.algorithm=PBEWITHHMACSHA384ANDAES_256  
-//             spring.datasource.password=ENC(G3WX2ZTaD52FyrEEZs/srX3QmsNhTlgO2vHnxVVCBDh1jDLDXhlqut) (contraseña generada anteriormente)  
+             jasypt.encryptor.algorithm=PBEWITHHMACSHA384ANDAES_256  
+             spring.datasource.password=ENC(G3WX2ZTaD52FyrEEZs/srX3QmsNhTlgO2vHnxVVCBDh1jDLDXhlqut) (contraseña generada anteriormente)  
 //         
 //         en la clase CrudNuevaArquitecturaApplication.java, agregar la propiedad 
-//         @EnableEncryptableProperties	
+           @EnableEncryptableProperties	
 //         
 //         Para esto se creo una utileria en la siguiente ruta la cual se encarga de encriptar y desencriptar 
 //         passwords mediante el algoritmo AESGCM => ruta donde se encuentra la  utilidad  
