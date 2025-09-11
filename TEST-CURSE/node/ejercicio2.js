3d17f2a6d8441b0364e6be300d452a239dd667e0
DGmike.dev <dgmike.dev@gmail.com>
2025-09-10 21:30:57 -0600

    test_curse

---
+++ TEST-CURSE/node/ejercicio2.js
@@ -0,0 +1,54 @@
+const readLiner = require('readline');
+const cls = require('child_process');
+
+const handlerln = readLiner.createInterface({
+    input:   process.stdin,
+    output : process.stdout
+});
+
+
+
+
+const url = 'https://bin-ip-checker.p.rapidapi.com/?bin=448590';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b6ed6fbefdmshbace003fa0e1d9ap1d2343jsn0f50daad9bb9',
+		'x-rapidapi-host': 'bin-ip-checker.p.rapidapi.com'
+	}
+};
+
+try {
+
+    fetch(url, options)
+    .then( resp => resp.json())
+    .then( response => {
+        console.log(response);
+    })
+	// const response = await fetch(url, options);
+	// const result = await response.text();
+	// console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+////// MENU  ///////////////////////////////
+
+const menu = `========================
+a) opcion 1
+b) opcion 2
+c) opcion 3
+d) opcion 4
+e) salir
+========================
+`;
+
+
+cls.exec('cls');
+
+handlerln.question(menu, (eleccion)=>{
+    
+    console.log("Opcion elegida (" + eleccion + ")");
+    handlerln.close();    
+});
