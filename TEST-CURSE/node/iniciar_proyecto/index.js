e319c28d3be49d2382aa5307d40d572f79ef201a
DGmike.dev <dgmike.dev@gmail.com>
2025-09-11 13:51:51 -0600

    curso node 3

---
+++ TEST-CURSE/node/iniciar_proyecto/index.js
@@ -0,0 +1,15 @@
+const handlerln = require('node:readline');
+
+// uso de colores
+const { yellow, blue, red, green, bggreen, bgred } = require('./utils/setColor.js');
+
+
+
+console.clear();
+console.log( yellow("==== Inicio de la aplicación ===="));
+console.log( blue("==== Inicio de la aplicación ===="));
+console.log( red("==== Inicio de la aplicación ===="));
+console.log( green("==== Inicio de la aplicación ===="));
+console.log( bggreen("==== Inicio de la aplicación ===="));
+console.log( bgred("==== Inicio de la aplicación ===="));
+console.log( "==== Inicio de la aplicación ====");
---
+++ TEST-CURSE/node/iniciar_proyecto/utils/setColor.js
@@ -0,0 +1,19 @@
+const pc = require('picocolors');
+
+
+const yellow = (data) => pc.yellow(data);
+const blue = (data) => pc.blueBright(data);
+const red = (data) => pc.red(data);
+const green = (data) => pc.green(data);
+
+const bggreen = (data) => pc.bgGreen(data);
+const bgred = (data) => pc.bgRed(data);
+
+module.exports = {
+    yellow,
+    blue,
+    red,
+    green,
+    bggreen,
+    bgred
+}
