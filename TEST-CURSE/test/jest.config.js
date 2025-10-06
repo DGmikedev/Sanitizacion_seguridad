c065f839e7f991fd481dfab95397da554c214d57
DGmike.dev <dgmike.dev@gmail.com>
2025-10-06 15:24:48 -0600

    config jest to reports

--- TEST-CURSE/test/jest.config.js
+++ TEST-CURSE/test/jest.config.js
@@ -1,5 +1,8 @@
 // jest.config.js
 export default {
   testEnvironment: "node",
-  transform: {}               // desactiva Babel si no lo usas
+  transform: {},               // desactiva Babel si no lo usas
+ collectCoverage: true,
+ coverageReporters: ["html", "text"],
+ coverageDirectory: "coverage"
 };
