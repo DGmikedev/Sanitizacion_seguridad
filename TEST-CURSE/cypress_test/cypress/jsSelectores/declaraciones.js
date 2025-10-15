8713c20d06ef615c15c783f81a1d27f1bf6a4e58
DGmike.dev <dgmike.dev@gmail.com>
2025-10-14 19:12:02 -0600

    start secc 2 ctpress

---
+++ TEST-CURSE/cypress_test/cypress/jsSelectores/declaraciones.js
@@ -0,0 +1,11 @@
+let strXML = " ===> <tag1>Etiqueta 1</tag1>"
+
+let objXML = {
+    tag:"tag_entrada",
+    tag2:"tag_entrada_2",
+    tag3:"tag_entrada_3"
+}
+
+console.log(typeof(strXML));
+console.log(typeof(objXML));
+console.log(objXML);
