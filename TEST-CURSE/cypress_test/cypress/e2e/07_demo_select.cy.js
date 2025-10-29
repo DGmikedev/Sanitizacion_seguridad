4fe0606ec5cf2c69b4ef49fe26410ee00bc94442
DGmike.dev <dgmike.dev@gmail.com>
2025-10-29 15:33:12 -0600

    data in form 3

--- TEST-CURSE/cypress_test/cypress/e2e/07_demo_select.cy.js
+++ TEST-CURSE/cypress_test/cypress/e2e/07_demo_select.cy.js
@@ -1,10 +1,14 @@
 import { formulario3 as frm3 } from './utils/formulario3_xpaths.js';
+import {faker} from '@faker-js/faker';
 import 'cypress-xpath';
+
+
 
 describe("DEMO 7", ()=>{
     it("COMBO SELECT", ()=>{
         cy.visit(frm3.Url);
         cy.xpath(frm3.name).type("EL MIKE")
+        cy.xpath(frm3.email).type()
         cy.wait(5000);
     });
 })
